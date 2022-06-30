5774d479a565a6772ef57b8acd4ecc14942249c6
Yasir Ado Hassan <ysquare.theimperial@gmail.com>
2022-06-30 09:04:28 +0100

    dropdown

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,26 +1,31 @@
-
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Col, Row } from "reactstrap";
-import ngd from "./ngd.jpg"
+import {
+  Card,
+  Col,
+  Dropdown,
+  DropdownMenu,
+  DropdownToggle,
+  Row,
+} from "reactstrap";
+import ngd from "./ngd.jpg";
 
 import "./navbar.css";
 
 export default function Home() {
+  const [open, setOpen] = useState(false);
+  const dropdown = () => {
+    setOpen(!open);
+  };
   const navigate = useNavigate();
   return (
     <div>
       <div className="nav-bar">
         <Row className="navbar-row">
-          <Col md={2} className="nav-items">
-            {/* <img src={logo} alt="sRecord" className="ln-logo" /> */}
-            <ul className="nav-list">
-              <a href="#features">
-                {/* <li className="l">Dating Site</li> */}
-              </a>
-            </ul>
+          <Col md={1} className="nav-items">
+            <p>Logo</p>
           </Col>
-          <Col md={7} className="nav-items">
+          <Col md={8} className="nav-items">
             <ul className="nav-list">
               <a href="#features">
                 <li className="l">66778</li>
@@ -40,12 +45,35 @@ export default function Home() {
               </a>
             </ul>
           </Col>
-          <Col md={3} className="div">
-            <div className="img1">
-              <img src={ngd} className='profile' />
-              <p style={{ display: 'inline', color: 'white' }}>{' '}Nagudun waka</p>
-            </div>
+          <Col md={2} className="div">
+            {/* <div className="img1">
+              <img src={ngd} className="profile" />
+              <p style={{ display: "inline", color: "white" }}> Nagudun waka</p>
+            </div> */}
+            <Dropdown isOpen={open} toggle={dropdown}>
+              <DropdownToggle
+                data-toggle="dropdown"
+                tag="span"
+                onClick={dropdown}
+              >
+                <h1 className="dp-name">
+                  <img src={ngd} className="dp" alt="profile" /> Khalifa Shuaibu
+                </h1>
+              </DropdownToggle>
+              <DropdownMenu className="shadow" style={{ border: "none" }}>
+                <div className="drop-p">
+                  <p style={{ margin: 8 }}>fasfsd</p>
+                </div>
+                <div className="drop-p">
+                  <p style={{ margin: 8 }}>fasfsd</p>
+                </div>
+                <div className="drop-p">
+                  <p style={{ margin: 8 }}>fasfsd</p>
+                </div>
+              </DropdownMenu>
+            </Dropdown>
           </Col>
+          <Col md={1}></Col>
         </Row>
       </div>
     </div>
